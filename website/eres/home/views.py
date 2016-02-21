10a076c1e09cd4802c3628fcf469e8be65a34f46
Hugo Possani <hpossani@localhost.localdomain>
2016-02-21 16:14:16 -0300

    Mudanca da pasta template

--- website/eres/home/views.py
+++ website/eres/home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Bem vindo ao Eres")
+   template = loader.get_template('home/index.html')
+   return HttpResponse(template.render(request))
