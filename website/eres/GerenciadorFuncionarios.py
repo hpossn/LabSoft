ecc5f56a88c3dd969642e5d586b794b798510a4e
Gabriel Casarin da Silva <gabriel.casarin@hotmail.com>
2016-05-27 00:30:21 -0300

    bugfix: agora o status da entrega muda de 'pendente' para 'em_transito' ao ser alocada a um entregador

--- website/eres/GerenciadorFuncionarios.py
+++ website/eres/GerenciadorFuncionarios.py
@@ -4,9 +4,17 @@ import home.models as models
 
 def alocarEntregaParaEntregador(entrega, entregador):
     if entrega.entregador is None:
+        # seta entregador como ocupado
         entregador.status = 1
-        entregador.save()
+
+        # altera o status da entrega para em transito
+        entrega.status = 'em_transito'
+
+        # faz a alocação propriamente dita
         entrega.entregador = entregador
+
+        # salva no banco de dados todas as alterações
+        entregador.save()
         entrega.save()
     else:
         print('entrega já tem entregador responsável por realizá-la')
