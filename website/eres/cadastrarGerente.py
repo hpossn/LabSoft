81feb54336f5f72a6deec0300a0eed81bdaaaf3f
Gabriel Casarin da Silva <gabriel.casarin@hotmail.com>
2016-06-22 21:29:58 -0300

    em cadastrarGerente.py
    associa gerente a um usuario

--- website/eres/cadastrarGerente.py
+++ website/eres/cadastrarGerente.py
@@ -12,7 +12,7 @@ def cadastrarGerente():
 
     gerente = models.Gerente(nome=nome, dataNascimento=datetime.strptime(dataNascimento, '%d/%m/%Y'), CPF=CPF, salario=float(salario))
     try:
-        gerente.save()
+        gerente.save(commit=False)
         username = input('username: ')
         senha = getpass.getpass('senha: ')
         senha_verificacao = getpass.getpass('digite a senha novamente: ')
@@ -20,7 +20,9 @@ def cadastrarGerente():
             senha = getpass.getpass('senha: ')
             senha_verificacao = getpass.getpass('digite a senha novamente: ')
         user = models.Usuario(username=username, password=senha, tipoUsuario=models.tiposDeUsuario['gerente'])
+        gerente.usuario = user
         user.save()
+        gerente.save()
     except Exception as e:
         print('nao foi possivel cadastrar novo gerente:', e)
         return
